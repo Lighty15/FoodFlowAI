c17b82f6077e5d88839ed508cb02aaea0a401a50
Lighty15 <catherinlightys@gmail.com>
2026-07-23 07:38:23 +0530

    Create database tables automatically

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,14 +1,21 @@
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
+
+from backend.app.db.base import Base
+from backend.app.db.session import engine
+
 from backend.app.api import donations, auth, admin
 
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI(title="FoodFlowAI Backend")
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://food-flow-82kx3ye8l-catherin.vercel.app",
+        "https://food-flow-ai-mu.vercel.app",  
     ],
     allow_credentials=True,
     allow_methods=["*"],
@@ -18,7 +25,6 @@ app.add_middleware(
 app.include_router(auth.router, prefix="/api/auth")
 app.include_router(donations.router, prefix="/api")
 app.include_router(admin.router, prefix="/api/admin")
-
 
 @app.get("/health")
 async def health():
