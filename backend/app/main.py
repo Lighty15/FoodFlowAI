a67a5756b1f2707a650df613a734b0c59f31c6dc
Lighty15 <catherinlightys@gmail.com>
2026-07-25 23:17:43 +0530

    Added register error logging

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,8 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from backend.app.db.session import initialize_database
 from backend.app.db import models
+from backend.app.db.session import SessionLocal
+from backend.app.services import auth_service
 
 # Import routers
 from backend.app.api import auth, donations, admin
@@ -10,9 +12,35 @@ from backend.app.api import auth, donations, admin
 app = FastAPI(title="FoodFlowAI Backend")
 
 
+
+
+
 @app.on_event("startup")
 async def startup_event():
     initialize_database()
+
+    db = SessionLocal()
+    try:
+        admin = db.query(models.User).filter(
+            models.User.username == "admin"
+        ).first()
+
+        if admin is None:
+            admin = models.User(
+                username="admin",
+                hashed_password=auth_service.get_password_hash("adminpass"),
+                role="admin",
+                is_active=1,
+            )
+            db.add(admin)
+            db.commit()
+            print("✅ Default admin created")
+        else:
+            print("✅ Admin already exists")
+    except Exception as e:
+        print("❌ Admin creation failed:", e)
+    finally:
+        db.close()
 
 app.add_middleware(
     CORSMiddleware,
